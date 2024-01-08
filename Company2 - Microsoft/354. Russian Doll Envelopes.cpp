b52a866ab97dac71347bea0e2339d94dc2e9d8ae
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-01-08 23:31:32 +0530

    Create 354. Russian Doll Envelopes.cpp

---
+++ Company2 - Microsoft/354. Russian Doll Envelopes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end(),[&](auto &a,auto &b){
+            return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+        });
+        
+        int n = arr.size();
+        
+        vector<vector<int>> dp;
+        
+        for(int i=0;i<n;i++){
+            auto lb = lower_bound(dp.begin(),dp.end(),arr[i] , [&] (auto &a, auto &b)
+            {
+                return (a[1] < b[1]) and (a[0] < b[0]);
+                });
+            if(lb == dp.end())
+                dp.push_back(arr[i]);
+            else
+                *lb = arr[i];
+        }
+        
+        return dp.size();
+    }
+};
+
+// Approach
+// Sort the array. Ascend on width and descend on height if width are same.
+// Find the longest increasing subsequence based on height of envelopea.
+// As you have arranges the height in decreasing sense for same width the code will automatically choose the least height for consideration ( read the link above).
