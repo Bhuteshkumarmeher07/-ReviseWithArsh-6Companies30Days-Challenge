87ab2f8c33672404dee660df8e601d564189486b
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-01-11 23:09:00 +0530

    Create 187. Repeated DNA Sequences.cpp

---
+++ Company2 - Microsoft/187. Repeated DNA Sequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n = s.length();
+        vector<string> ans;
+
+        unordered_map<string, int> m;
+        for(int i=0;i<=n-10;i++){
+            string str = s.substr(i,10);
+            m[str]++;
+        }
+
+        for(auto it:m){
+            if(it.second>1){
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+};
